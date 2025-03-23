6a82edb3b6fb910ab7045f726d3a26234ee1d476
je0ng3 <jepark2934@gmail.com>
2025-03-23 20:18:45 +0900

    [Feat] add docker-compose.yml

--- src/main/java/com/capstone/favicon/config/RedisConfig.java
+++ src/main/java/com/capstone/favicon/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.capstone.favicon.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,17 +15,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @RequiredArgsConstructor
 @EnableRedisRepositories
+@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
     private String host;
 
     @Value("${spring.data.redis.port}")
-    private String port;
+    private Integer port;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, Integer.parseInt(port));
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
